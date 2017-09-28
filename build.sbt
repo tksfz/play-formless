d3445410412c106347609ef7f0d8830870878486
Thomas Kim <tk@iterable.com>
2017-09-28 13:59:57 -0700

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+libraryDependencies ++= Seq(
+  "com.typesafe.play" %% "play" % "2.4.8",
+  "com.chuusai" %% "shapeless" % "2.3.2"
+)
---
+++ src/main/scala/Mapper.scala
@@ -0,0 +1,99 @@
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraint
+import shapeless._
+import shapeless.labelled._
+
+trait Mapper[T] {
+  type Out
+  def apply(t: T): Mapping[Out]
+}
+
+object Mapper {
+
+  type Aux[A, B] = Mapper[A] { type Out = B }
+
+  /**
+    * Instances for Mappers that _accept_ records with key ->> mapper
+    * and _output_ mappings of HList records
+    */
+
+  /**
+    * Mapper for record elements that supply the Mapping[T] directly:
+    *     'foo ->> nonEmptyText
+    */
+  implicit def h1[K <: Symbol, T]
+  (implicit wk: Witness.Aux[K]): Mapper[FieldType[K, Mapping[T]]] = new Mapper[FieldType[K, Mapping[T]]] {
+    type Out = FieldType[K, T]
+    override def apply(t: FieldType[K, Mapping[T]]): Mapping[FieldType[K, T]] = {
+      // Since FieldType[K, V] == V tagged with K, the transformation is trivial
+      t.withPrefix(wk.value.name).transform(t => t.asInstanceOf[FieldType[K, T]], identity)
+    }
+  }
+
+  // Note there's no instance for HNil
+
+  implicit def hsingle[K <: Symbol, T]
+  (implicit
+    wk: Witness.Aux[K],
+   h1: Mapper[FieldType[K, Mapping[T]]]
+  ): Mapper[FieldType[K, Mapping[T]] :: HNil] = new Mapper[FieldType[K, Mapping[T]] :: HNil] {
+    type Out = FieldType[K, T] :: HNil
+
+    override def apply(t: FieldType[K, Mapping[T]] :: HNil): Mapping[FieldType[K, T] :: HNil] = {
+      t.head.transform(t => t.asInstanceOf[FieldType[K, T]] :: HNil, b => b.head)
+    }
+  }
+
+  implicit def hcons[K <: Symbol, S, SO, T <: HList, TO <: HList]
+  (implicit
+    single: Mapper.Aux[FieldType[K, Mapping[S]], SO],
+    lmapper: Mapper.Aux[T, TO]
+  ): Mapper[FieldType[K, Mapping[S]] :: T] = new Mapper[FieldType[K, Mapping[S]] :: T] {
+    override type Out = SO :: TO
+
+    override def apply(t: FieldType[K, Mapping[S]] :: T): Mapping[SO :: TO] = {
+      val smapping: Mapping[SO] = single.apply(t.head)
+      val lmapping = lmapper.apply(t.tail)
+      new ConsMapping(smapping, lmapping)
+    }
+  }
+
+  class ConsMapping[H, T <: HList](hmapping: Mapping[H], tmapping: Mapping[T], val key: String = "", val constraints: Seq[Constraint[H :: T]] = Nil)
+    extends Mapping[H :: T] {
+
+    private[this] val hfield = hmapping.withPrefix(key)
+    private[this] val tfields = tmapping.withPrefix(key)
+
+    override val mappings = Seq(this) ++ hfield.mappings ++ tfields.mappings
+
+    override def bind(data: Map[String, String]): Either[Seq[FormError], H :: T] = {
+      (hfield.bind(data), tfields.bind(data)) match {
+        case (Left(errors1), Left(errors2)) => Left(errors1 ++ errors2)
+        case (Left(errors1), _) => Left(errors1)
+        case (_, Left(errors2)) => Left(errors2)
+        case (Right(s), Right(l)) => Right(s :: l)
+      }
+    }
+
+    override def unbind(value: H :: T): Map[String, String] = {
+      hfield.unbind(value.head) ++ tfields.unbind(value.tail)
+    }
+
+    override def unbindAndValidate(value: H :: T): (Map[String, String], Seq[FormError]) = {
+      val (m1, e1) = hfield.unbindAndValidate(value.head)
+      val (m2, e2) = tfields.unbindAndValidate(value.tail)
+      (m1 ++ m2, e1 ++ e2)
+    }
+
+    override def withPrefix(prefix: String) = {
+      addPrefix(prefix).map { newKey =>
+        new ConsMapping(hfield, tfields, newKey, constraints)
+      }.getOrElse(this)
+    }
+
+    override def verifying(constraints: Constraint[H :: T]*) = {
+      new ConsMapping(hfield, tfields, key, this.constraints ++ constraints.toSeq)
+    }
+  }
+}
