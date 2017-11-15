ad3f331d33a3ae0209a7bb0b8b3ec3e4d5e44c83
Charles Ruhland <charles@iterable.com>
2017-11-14 16:45:06 -0800

    Fix compile issues; defaults in traits

--- src/main/scala/com/iterable/formless/RefinedConstraint.scala
+++ src/main/scala/com/iterable/formless/RefinedConstraint.scala
@@ -18,9 +18,10 @@ import play.api.data.validation.Valid
 import play.api.data.validation.ValidationError
 import shapeless.Nat
 import shapeless.Witness
+import shapeless.ops.nat.ToInt
 
 trait RefinedConstraint[T, P] {
-  def apply: Constraint[T]
+  def constraint: Constraint[T]
 }
 
 object RefinedConstraint {
@@ -30,11 +31,11 @@ object RefinedConstraint {
   implicit def not[T, P](implicit rc: RefinedConstraint[T, P]): RefinedConstraint[T, Not[P]] = {
     val name = "refined.not"
     new RefinedConstraint[T, Not[P]] {
-      def apply: Constraint[T] = {
+      val constraint: Constraint[T] = {
         Constraint(name) { t =>
-          val c = rc()
+          val c = rc.constraint
           c(t) match {
-            case Valid => Invalid(ValidationError(name, c.name, c.args: _*))
+            case Valid => Invalid(ValidationError(name, c.name +: c.args: _*))
             case Invalid(_) => Valid
           }
         }
@@ -47,9 +48,9 @@ object RefinedConstraint {
     rc2: RefinedConstraint[T, P2]
   ): RefinedConstraint[T, P1 And P2] = {
     new RefinedConstraint[T, P1 And P2] {
-      def apply: Constraint[T] = {
+      val constraint: Constraint[T] = {
         Constraint("refined.and") { t =>
-          (rc1()(t), rc2()(t)) match {
+          (rc1.constraint(t), rc2.constraint(t)) match {
             case (Valid, Valid) => Valid
             case (Valid, inv @ Invalid(_)) => inv
             case (inv @ Invalid(_), Valid) => inv
@@ -65,11 +66,11 @@ object RefinedConstraint {
     rc2: RefinedConstraint[T, P2]
   ): RefinedConstraint[T, P1 Or P2] = {
     new RefinedConstraint[T, P1 Or P2] {
-      def apply: Constraint[T] = {
+      val constraint: Constraint[T] = {
         Constraint("refined.or") { t =>
-          rc1()(t) match {
+          rc1.constraint(t) match {
             case Valid => Valid
-            case inv1 @ Invalid(_) => rc2()(t) match {
+            case inv1 @ Invalid(_) => rc2.constraint(t) match {
               case Valid => Valid
               case inv2 @ Invalid(_) => inv1 ++ inv2
             }
@@ -81,24 +82,27 @@ object RefinedConstraint {
 
   // numeric //
 
-  implicit def less[N]: RefinedConstraint[Int, Less[N]] = {
+  implicit def less[N <: Nat](implicit n: ToInt[N]): RefinedConstraint[Int, Less[N]] = {
     new RefinedConstraint[Int, Less[N]] {
-      def apply: Constraint[Int] = Constraints.max(Nat.toInt[N], strict = true)
+      val constraint: Constraint[Int] = Constraints.max(n(), strict = true)
     }
   }
 
-  implicit def greater[N]: RefinedConstraint[Int, Greater[N]] = {
+  implicit def greater[N <: Nat](implicit n: ToInt[N]): RefinedConstraint[Int, Greater[N]] = {
     new RefinedConstraint[Int, Greater[N]] {
-      def apply: Constraint[Int] = Constraints.min(Nat.toInt[N], strict = true)
+      val constraint: Constraint[Int] = Constraints.min(n(), strict = true)
     }
   }
 
-  implicit def modulo[N, O]: RefinedConstraint[Int, Modulo[N, O]] = {
+  implicit def modulo[N <: Nat, M <: Nat](implicit
+    n: ToInt[N],
+    m: ToInt[M]
+  ): RefinedConstraint[Int, Modulo[N, M]] = {
     val name = "refined.modulo"
-    new RefinedConstraint[Int, Modulo[N, O]] {
-      def apply: Constraint[Int] = {
+    new RefinedConstraint[Int, Modulo[N, M]] {
+      val constraint: Constraint[Int] = {
         Constraint("refined.modulo") { t =>
-          if (t % Nat.toInt[N] == Nat.toInt[O]) Valid else Invalid(ValidationError(name))
+          if (t % n() == m()) Valid else Invalid(ValidationError(name))
         }
       }
     }
@@ -109,7 +113,7 @@ object RefinedConstraint {
   implicit def empty: RefinedConstraint[String, Empty] = {
     val name = "refined.empty"
     new RefinedConstraint[String, Empty] {
-      def apply: Constraint[String] = {
+      val constraint: Constraint[String] = {
         Constraint(name) { t =>
           if (t.isEmpty) Valid else Invalid(ValidationError(name))
         }
@@ -119,19 +123,19 @@ object RefinedConstraint {
 
   implicit def nonEmpty: RefinedConstraint[String, NonEmpty] = {
     new RefinedConstraint[String, NonEmpty] {
-      def apply: Constraint[String] = Constraints.nonEmpty
+      val constraint: Constraint[String] = Constraints.nonEmpty
     }
   }
 
-  implicit def minSize[N]: RefinedConstraint[String, MinSize[N]] = {
+  implicit def minSize[N <: Nat](implicit n: ToInt[N]): RefinedConstraint[String, MinSize[N]] = {
     new RefinedConstraint[String, MinSize[N]] {
-      def apply: Constraint[String] = Constraints.minLength(Nat.toInt[N])
+      val constraint: Constraint[String] = Constraints.minLength(n())
     }
   }
 
-  implicit def maxSize[N]: RefinedConstraint[String, MaxSize[N]] = {
+  implicit def maxSize[N <: Nat](implicit n: ToInt[N]): RefinedConstraint[String, MaxSize[N]] = {
     new RefinedConstraint[String, MaxSize[N]] {
-      def apply: Constraint[String] = Constraints.maxLength(Nat.toInt[N])
+      val constraint: Constraint[String] = Constraints.maxLength(n())
     }
   }
 
@@ -139,7 +143,7 @@ object RefinedConstraint {
     w: Witness.Aux[S]
   ): RefinedConstraint[String, MatchesRegex[S]] = {
     new RefinedConstraint[String, MatchesRegex[S]] {
-      def apply: Constraint[String] = Constraints.pattern(w.value.r)
+      val constraint: Constraint[String] = Constraints.pattern(w.value.r)
     }
   }
 
